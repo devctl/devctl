9e08a4e95562edf960d0931d7986c290611cb017
Benjamin Caldwell <benjamincaldwell@users.noreply.github.com>
2016-12-30 20:47:51 -0500

    Update devctl.sh

--- devctl.sh
+++ devctl.sh
@@ -118,7 +118,9 @@ _devctl_install_version() {
   local remote_hash=$(wget -qO- "${website}/dl/sha/${system_name}" | grep "${1}" | perl -e 'if (<> =~ /$1:([a-fA-F\d]{32})/g) {print "$1"} else {print <>}')
   if _devctl_verify_hash "${tar_file_name}" "${remote_hash}"
   then
-    /bin/rm -r "${install_location}"/devctl/*
+    if [[ -d "${install_location}"/devctl]]; then
+      /bin/rm -r "${install_location}"/devctl/*
+    fi
     tar -zxvf "${tar_file_name}" -C "${install_location}" --keep-newer-files
     chmod +x "${install_location}/devctl/devctl"
     /bin/rm -r "${tar_file_name}"
